6c0344417529a4e5fed0799c5d4f464d59c5e884
zakkor <edward.partenie@gmail.com>
2020-05-22 13:52:36 +0300

    responsive fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
 	theme: {
+		container: {
+      padding: {
+        default: '1rem',
+      },
+    },
 		fontFamily: {
 			sans: ['Inter', 'sans-serif'],
 			display: ['Inter', 'sans-serif'],
